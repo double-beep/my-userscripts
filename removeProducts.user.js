df3f9034cfe729ddf6a4f5ad621f997687a3db24
Double Beep <38133098+double-beep@users.noreply.github.com>
2019-10-23 20:59:01 +0300

    Upload the userscript that removed the Products button

---
+++ removeProducts.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Remove the Products button
+// @version      v1.2.0
+// @description  Remove the annoying Products button Stack Overflow added in a horrible attempt to earn $$$, which probably ain't gonna do much aside piss off core users
+// @author       double-beep
+// @include      *://stackoverflow.com/*
+// ==/UserScript==
+
+(function removeProductsButton() {
+    $("ol").remove(".list-reset, .grid, .gs4")
+})();
