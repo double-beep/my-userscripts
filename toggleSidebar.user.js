b8c34b24668c823d76acff895f563a322a8ddb2c
double beep <38133098+double-beep@users.noreply.github.com>
2021-04-27 09:37:47 +0300

    Reduce jQuery usage

--- toggleSidebar.user.js
+++ toggleSidebar.user.js
@@ -1,39 +1,35 @@
 // ==UserScript==
 // @name         Toggle the right sidebar
-// @version      0.4
+// @version      0.5.0
 // @author       double-beep
-// @include      /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/(questions/\d+/|review/*)
-// @exclude      /^https://*.*/review/(suggested-edits|reopen)/*
-// @exclude      /^https://*.*/review($|.*/(history|stats)$)
+// @include      /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/questions/\d+/
 // @updateURL    https://github.com/double-beep/my-userscripts/raw/master/toggleSidebar.user.js
+// @downloadURL  https://github.com/double-beep/my-userscripts/raw/master/toggleSidebar.user.js
 // @grant        none
-// @run-at       document-end
 // ==/UserScript==
 
-(function($) {
-  $('.-secondary').append('<li class="-item"><a class="-link" id="hide-sidebar" title="Toggle visibility of right sidebar">&#10095;</a></li>');
+(function() {
+  const buttonWrapper = document.createElement('li');
+  buttonWrapper.classList.add('-item');
+  const toggleButton = document.createElement('a');
+  toggleButton.classList.add('-link');
+  toggleButton.id = 'hide-sidebar';
+  toggleButton.title = 'Toggle visibility of right sidebar';
+  toggleButton.innerHTML = '&#10095;';
+  buttonWrapper.appendChild(toggleButton);
+  document.querySelector('.-secondary').appendChild(buttonWrapper);
 
-  function defineOnClick() {
-    const sidebar = $('#sidebar, .sidebar'), mainbar = $('#mainbar, .mainbar');
-    $(document).on('click', '#hide-sidebar', function() {
-      if (sidebar.is(':visible')) {
-        sidebar.hide(200);
-        mainbar.animate({'width': '100%'}, 200);
-        $(this).html('&#10094;');
-      } else {
-        sidebar.show(200);
-        mainbar.css('width', 'calc(-324px + 100%)');
-        $(this).html('&#10095;');
-      }
-    });
-  }
-  if (window.location.pathname.match(/\/questions\/\d+/)) defineOnClick();
-
-  var origOpen = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function() {
-    this.addEventListener('load', () => {
-      if (this.responseURL.match(/\/review\/(next-task|task-reviewed)/)) defineOnClick();
-    });
-    origOpen.apply(this, arguments);
-  };
-})(jQuery);
+  const sidebar = $(document.querySelector('#sidebar, .sidebar')), mainbar = document.querySelector('#mainbar, .mainbar');
+  document.querySelector('#hide-sidebar').addEventListener('click', event => {
+    const toggleButton = event.target;
+    if (sidebar.is(':visible')) {
+      sidebar.hide(200);
+      $(mainbar).animate({ width: '100%' }, 200);
+      toggleButton.innerHTML = '&#10094;';
+    } else {
+      sidebar.show(200);
+      mainbar.style.width = 'calc(-324px + 100%)';
+      toggleButton.innerHTML = '&#10095;';
+    }
+  });
+})();
