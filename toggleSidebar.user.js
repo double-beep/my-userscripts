77b7dd38b6936d2ee23067f918b8a80972aaf3e6
double beep <38133098+double-beep@users.noreply.github.com>
2022-03-31 15:01:39 +0300

    1.5.2: Adapt to .top-bar → .s-topbar change
    Also change to 4-space indentation

--- toggleSidebar.user.js
+++ toggleSidebar.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         Toggle the right sidebar
-// @version      0.5.1
+// @version      0.5.2
 // @author       double-beep
+// @contributor  Scratte
+// @description  Add the ability to toggle the right sidebar
 // @include      /^https://(?:[^/]+\.)?(?:(?:stackoverflow|serverfault|superuser|stackexchange|askubuntu|stackapps)\.com|mathoverflow\.net)/*
 // @exclude      https://*.*/review*
 // @updateURL    https://github.com/double-beep/my-userscripts/raw/master/toggleSidebar.user.js
@@ -11,27 +13,30 @@
 // ==/UserScript==
 
 (function() {
-  const buttonWrapper = document.createElement('li');
-  buttonWrapper.classList.add('-item');
-  const toggleButton = document.createElement('a');
-  toggleButton.classList.add('-link');
-  toggleButton.id = 'hide-sidebar';
-  toggleButton.title = 'Toggle visibility of right sidebar';
-  toggleButton.innerHTML = '&#10095;';
-  buttonWrapper.appendChild(toggleButton);
-  document.querySelector('.-secondary').appendChild(buttonWrapper);
+    const buttonWrapper = document.createElement('li');
 
-  const sidebar = $(document.querySelector('#sidebar, .sidebar')), mainbar = document.querySelector('#mainbar, .mainbar');
-  document.querySelector('#hide-sidebar').addEventListener('click', event => {
-    const toggleButton = event.target;
-    if (sidebar.is(':visible')) {
-      sidebar.hide(200);
-      $(mainbar).animate({ width: '100%' }, 200);
-      toggleButton.innerHTML = '&#10094;';
-    } else {
-      sidebar.show(200);
-      mainbar.style.width = 'calc(-324px + 100%)';
-      toggleButton.innerHTML = '&#10095;';
-    }
-  });
+    const toggleButton = document.createElement('a');
+    toggleButton.id = 'hide-sidebar';
+    toggleButton.title = 'Toggle visibility of right sidebar';
+    toggleButton.innerHTML = '&#10095;';
+    toggleButton.classList.add('s-topbar--item');
+
+    buttonWrapper.appendChild(toggleButton);
+
+    document.querySelector('.s-topbar--content').appendChild(buttonWrapper);
+
+    const sidebar = $(document.querySelector('#sidebar, .sidebar'));
+    const mainbar = document.querySelector('#mainbar, .mainbar');
+
+    document.querySelector('#hide-sidebar').addEventListener('click', ({ target }) => {
+        if (sidebar.is(':visible')) {
+            sidebar.hide(200);
+            $(mainbar).animate({ width: '100%' }, 200);
+            target.innerHTML = '&#10094;';
+        } else {
+            sidebar.show(200);
+            mainbar.style.width = 'calc(-324px + 100%)';
+            target.innerHTML = '&#10095;';
+        }
+    });
 })();
